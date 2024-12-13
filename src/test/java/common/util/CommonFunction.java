c7afb5a302b250d275e0e1788cdb909c4b4a6e8e
lckdevelop <lckdevelop@gmail.com>
2024-12-14 08:47:28 +0900

    패키징화

---
+++ src/test/java/common/util/CommonFunction.java
@@ -0,0 +1,13 @@
+package common.util;
+
+public class CommonFunction {
+
+    public void getTerm(int milliseconds) {
+        try {
+            Thread.sleep(milliseconds);
+            System.out.println(milliseconds + "초가 지난 후 실행");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
